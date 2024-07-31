3db1096daf8f46808b1189773a16d40945e52943
Mohammed Asad <mohammedasad7944@gmail.com>
2024-07-31 18:39:21 +0530

    Update music_recommendation.py

--- sections/music_recommendation.py
+++ sections/music_recommendation.py
@@ -45,23 +45,26 @@ def music_recommendation():
     selected_movie = st.selectbox("Select a song from the dropdown", music_list,index=None)
 
     if st.button('Recommend'):
-        recommended_music_names,recommended_music_posters = recommend(selected_movie)
-        col1, col2, col3, col4, col5= st.columns(5)
-        with col1:
-            st.text(recommended_music_names[0])
-            st.image(recommended_music_posters[0])
-        with col2:
-            st.text(recommended_music_names[1])
-            st.image(recommended_music_posters[1])
-
-        with col3:
-            st.text(recommended_music_names[2])
-            st.image(recommended_music_posters[2])
-        with col4:
-            st.text(recommended_music_names[3])
-            st.image(recommended_music_posters[3])
-        with col5:
-            st.text(recommended_music_names[4])
-            st.image(recommended_music_posters[4])
+        if selected_movie:
+            recommended_music_names,recommended_music_posters = recommend(selected_movie)
+            col1, col2, col3, col4, col5= st.columns(5)
+            with col1:
+                st.text(recommended_music_names[0])
+                st.image(recommended_music_posters[0])
+            with col2:
+                st.text(recommended_music_names[1])
+                st.image(recommended_music_posters[1])
+    
+            with col3:
+                st.text(recommended_music_names[2])
+                st.image(recommended_music_posters[2])
+            with col4:
+                st.text(recommended_music_names[3])
+                st.image(recommended_music_posters[3])
+            with col5:
+                st.text(recommended_music_names[4])
+                st.image(recommended_music_posters[4])
+        else:
+            st.warning('Please select a song before clicking the recommend button.')
 
 music_recommendation()
